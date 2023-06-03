7d4bf2ee5d1b23a2155ea83d62bc933631454351
Payton <payton.slim@gmail.com>
2023-06-02 17:02:40 -0700

    asdf

--- api.js
+++ api.js
@@ -57,6 +57,6 @@ app.all('/items*', (req, res, next) => {
 });
 
 // Start the server
-app.listen(80, () => {
-  console.log('Server started on port 80');
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
