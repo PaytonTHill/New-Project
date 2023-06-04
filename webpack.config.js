4eb8215a3bdb410e94c0a7043086797d14d99592
Payton <payton.slim@gmail.com>
2023-06-04 07:10:02 -0700

    added babel

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './Portfolio/public/reference.js', // Update with the path to your reference.js file
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory for compiled files
+    filename: 'bundle.js', // Output filename
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Optional: Enable importing files without specifying the file extensions
+  },
+};
