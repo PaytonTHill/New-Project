e5fab225e9c5dce77e157f25c77a449e75c1c27a
Payton <payton.slim@gmail.com>
2023-06-04 10:34:16 -0700

    explicit local host

--- api.js
+++ api.js
@@ -109,6 +109,6 @@ app.all('/items*', (req, res, next) => {
 });
 
 // Start the server
-app.listen(3000, () => {
+app.listen(3000, 'localhost' , () => {
   console.log('Server started on port 3000');
 });
